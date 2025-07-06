71acbf764da3de163337073dc013665f332c37b2
dragonblade316 <dragonblade316@gmail.com>
2025-07-05 19:47:00 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use std::{env, path::PathBuf};
+use clap::Parser;
+use anyhow::Result;
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Debug, Deserialize, Clone)]
+struct Movement {
+    target: PathBuf,
+    destination: PathBuf
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// target dirrectory
+    #[arg(short, long)]
+    config: Option<PathBuf>,
+
+    /// output dirrectory
+    #[arg(short, long)]
+    output: PathBuf,
+
+    // #[arg(short, long, default_value="true")]
+    // destructive: bool
+}
+
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let working_dir = env::current_dir()?;
+
+    let target_dir = match args.config {
+        Some(c) => {
+            if c.is_absolute() {
+                c
+            } else {
+                working_dir.join(c)
+            }
+        },
+        None => working_dir.clone(),
+    };
+
+    let config: Vec<Movement> = serde_json::from_str(fs::read_to_string(&target_dir.join("t-copy.json")).expect("t-copy.json not found").as_str())?;
+
+
+    let output_dir = match args.output.is_absolute() {
+        true => args.output,
+        false => working_dir.join(args.output),
+    };
+
+    // print!("{:?}", working_dir.join(config.get(0).unwrap().destination.clone()));
+
+    for i in config {
+
+        let tpath = target_dir.join(i.target.clone());
+        let dpath = output_dir.join(i.destination.clone());
+
+        println!("Copying {:?} to {:?}", tpath, dpath);
+
+        if !tpath.exists() {
+            eprintln!("{} not found", tpath.to_str().expect("non unicode paths not supported"));
+            continue;
+        }
+
+        if tpath.is_dir() {
+            if !dpath.exists() {
+                let _ = fs_extra::dir::create_all(dpath.clone(), false)?;
+            }
+
+            let mut options = fs_extra::dir::CopyOptions::new();
+            options.content_only = true;
+            options.overwrite = true;
+            let _ = fs_extra::dir::copy(tpath, dpath, &options)?;
+            continue;
+        }
+
+        if tpath.is_file() {
+            let mut testpath = dpath.clone();
+            testpath.pop();
+            if !testpath.exists() {
+                let _ = fs_extra::dir::create_all(testpath, false)?;
+            }
+
+            let options = fs_extra::file::CopyOptions::new().overwrite(true);
+            let _ = fs_extra::file::copy(tpath, dpath, &options)?;
+            continue;
+        }
+
+    }
+    
+    Ok(())
+}
